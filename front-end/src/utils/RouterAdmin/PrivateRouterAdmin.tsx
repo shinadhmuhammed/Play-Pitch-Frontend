fdd462dd36d0d8ff15c29b4097c9de664dcd6a52
muhammed shinadh <muhammedshinad2@gmail.com>
2024-04-08 15:49:32 +0530

    added private and public router for admin based on authentication

---
+++ front-end/src/utils/RouterAdmin/PrivateRouterAdmin.tsx
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+function PrivateRouterAdmin() {
+    const userToken = localStorage.getItem('adminToken');
+    const isAuthenticated = userToken ? true : false;
+    return isAuthenticated ? <Outlet /> : <Navigate to="/admin/adminlogin" />;
+}
+
+export default PrivateRouterAdmin
---
+++ front-end/src/utils/RouterAdmin/PublicRouterAdmin.tsx
@@ -0,0 +1,12 @@
+
+import { Outlet, Navigate } from "react-router-dom";
+
+
+function PublicRouterAdmin() {
+    const userToken = localStorage.getItem('adminToken');
+    const isAuthenticated = userToken ? true : false;
+
+    return isAuthenticated ?<Navigate to="/admin/dashboard" />: <Outlet /> 
+}
+
+export default PublicRouterAdmin
