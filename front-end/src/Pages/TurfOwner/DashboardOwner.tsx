115b6c8a3cc98f463ab2c7ed41aedcfb0a92bc4a
muhammed shinadh <muhammedshinad2@gmail.com>
2024-04-25 13:33:17 +0530

    Added dashboard page for the owner that shows daily,montly and weekly income and booking based on specific user

---
+++ front-end/src/Pages/TurfOwner/DashboardOwner.tsx
@@ -0,0 +1,12 @@
+
+import OwnerDashboard from '../../components/TurfOwner/OwnerDashboard'
+
+function DashboardOwner() {
+  return (
+    <div>
+      <OwnerDashboard/>
+    </div>
+  )
+}
+
+export default DashboardOwner
